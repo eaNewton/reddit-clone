ff3e500c10d6b978665f1157344af73e591b3c78
e. a. newton <emmettnewton1@gmail.com>
2018-10-06 12:02:10 -0500

    resolve package issues, link firebase

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCBHO4M8PPI8ugR6WzkFR1_QZHwUOW5P-4",
+  authDomain: "reddit-clone-527c3.firebaseapp.com",
+  databaseURL: "https://reddit-clone-527c3.firebaseio.com",
+  projectId: "reddit-clone-527c3",
+  storageBucket: "reddit-clone-527c3.appspot.com",
+  messagingSenderId: "721320746993"
+};
